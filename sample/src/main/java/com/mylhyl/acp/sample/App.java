0af65bd58cbe952e4e19b3b28fc07116b9fde1ae
石头 <48025211@qq.com>
2018-04-25 16:36:36 +0800

    更换bintray插件

--- sample/src/main/java/com/mylhyl/acp/sample/App.java
+++ sample/src/main/java/com/mylhyl/acp/sample/App.java
@@ -7,10 +7,15 @@ import com.squareup.leakcanary.LeakCanary;
 /**
  * Created by hupei on 2016/9/5.
  */
-public class App extends Application{
+public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
         LeakCanary.install(this);
     }
 }
